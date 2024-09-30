204f9e4d6d9e2579b74a538d9f21afba52a372f4
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2024-09-30 23:35:59 +0530

    Add files via upload
    first array project

---
+++ src/main/java/ArrayProject1.java
@@ -0,0 +1,32 @@
+package com.madhu.array;
+
+import java.util.Scanner;
+
+public class ArrayProject1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many days temperature : ");
+		int numOfDays = sc.nextInt();
+		int[] tempAbove = new int[numOfDays];
+		int sum = 0;
+
+		for (int i = 0; i < numOfDays; i++) {
+			System.out.print("Day " + (i + 1) + "'s temp : ");
+			tempAbove[i] = sc.nextInt();
+			sum += tempAbove[i];
+		}
+
+		double avg = sum / numOfDays;
+		System.out.println("Average = " + avg);
+
+		int above = 0;
+		for (int i = 0; i < tempAbove.length; i++) {
+			if (tempAbove[i] > avg) {
+				above++;
+			}
+		}
+		System.out.println(above + " days above temperature");
+	}
+
+}
