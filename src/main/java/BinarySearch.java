16df9ef9024e7dec109e77fa2d9a31af15fb7f0f
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2024-10-13 21:20:04 +0530

    Binary search using recursive method and in build method.

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,12 +1,47 @@
 package main.java;
 
 public class BinarySearch {
-
+	
 	public static void main(String[] args) {
 		int[] arr = new int[] {1,2,3,4,5};
 		int key = 5;
-		int index = IterativeBinarySearch(arr, 0, arr.length, key);
-		System.out.println("Key : "+key+" found at index : "+index + " using iterative method.");
+		
+		//there are 3 methods in binary search ie, Iterative method, Recursive method and In-build method
+		int iterativeIndex = IterativeBinarySearch(arr, 0, arr.length-1, key);
+		if(iterativeIndex >= 0) {
+			System.out.println("Key : "+key+" found at index : "+ iterativeIndex + " using iterative method.");
+		}else {
+			System.out.println("Element not found.");
+		}
+		
+		int recursiveIndex = recursiveBinarySearch(arr, 0, arr.length-1, key);
+		if(recursiveIndex >= 0) {
+			System.out.println("Key : "+key+" found at index : "+ recursiveIndex + " using recursive method.");
+		}else {
+			System.out.println("Element not found.");
+		}
+		
+		int inBuildIndex = Arrays.binarySearch(arr, key);
+		if(inBuildIndex >= 0) {
+			System.out.println("Key : "+key+" found at index : "+ inBuildIndex + " using in build method.");
+		}else {
+			System.out.println("Element not found.");
+		}
+		
+	}
+
+	private static int recursiveBinarySearch(int[] arr, int low, int high, int key) {
+		if(low<=high) {
+			int mid = low+(high-low)/2;
+			if(arr[mid]==key) {
+				return mid;
+			}
+			if(arr[mid]<key) {
+				return recursiveBinarySearch(arr, mid+1, high, key);
+			}
+			return recursiveBinarySearch(arr, low, mid-1, key);
+		}
+		return -1;
 	}
 
 	private static int IterativeBinarySearch(int[] arr, int low, int high, int key) {
